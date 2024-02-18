5ee9728b51724c9f242bc4d17539a709bf1545ea
Lara <laralim10@gmail.com>
2024-02-18 16:40:30 -0500

    Added all verifications and new credit card fields

--- scripts/service.js
+++ scripts/service.js
@@ -1,7 +1,67 @@
+function validateName(txtName) {
+    var a = document.getElementById(txtName).value;
+    // This filter asks for only alphabetical characters for the name
+    var filter = /^[A-Za-z]+$/;
+    if (filter.test(a)) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+function validateEmail(txtEmail) {
+    var a = document.getElementById(txtEmail).value;
+    // This filter asks for the characters '@' and '.' to be included in the email
+    var filter = /^[A-Za-z]+\@[A-Za-z]+\.[A-Za-z]+$/;
+    if (filter.test(a)) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
 function validatePhone(txtPhone) {
     var a = document.getElementById(txtPhone).value;
     // This filter asks for the format ###-###-#### for the phone number
     var filter = /^(?:\d{3}|\(\d{3}\))([-/.])\d{3}\1\d{4}$/;
+    if (filter.test(a)) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+function validateCreditCard(txtCreditCard) {
+    var a = document.getElementById(txtCreditCard).value;
+    // This filter asks for the format #### #### #### #### for the credit card number
+    var filter = /^\d{4} \d{4} \d{4} \d{4}$/;
+    if (filter.test(a)) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+function validateCVV(txtCVV) {
+    var a = document.getElementById(txtCVV).value;
+    // This filter asks for the format ### for the CVV
+    var filter = /^\d{3}$/;
+    if (filter.test(a)) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+function validateExpDate(txtExpDate) {
+    var a = document.getElementById(txtExpDate).value;
+    // This filter asks for the format MM/YY for the credit card expiration date
+    var filter = /^\d{2}\/\d{2}$/;
     if (filter.test(a)) {
         return true;
     }
@@ -29,18 +89,63 @@ function disableDates(date) {
 // HERE, JQuery "LISTENING" starts
 $(document).ready(function(){
 
-    // phone validation, it calls validatePhone
-    // and also some feedback as an Alert + putting a value in the input that shows the format required
-    // the "addClass" will use the class "error" defined in style.css and add it to the phone input
-    // The "error" class in style.css defines yellow background and red foreground
-    $("#customer-phone").on("change", function(){
-        if (!validatePhone("customer-phone")){
-            alert("Wrong format for phone number. Please re-enter.");
-            $("#customer-phone").val("");
-            $("#customer-phone").addClass("error");
+    $("#customer-name").on("change", function(){ // calls validateName and displays error alert if not valid
+        if (!validateName("customer-name")){
+            $("#customer-name-alert").removeClass("d-none");
+            $("#customer-name").val("");
         }
         else {
-            $("#customer-phone").removeClass("error");
+            $("#customer-name-alert").addClass("d-none");
+        }
+    });
+
+    $("#customer-email").on("change", function(){ // calls validateEmail and displays error alert if not valid
+        if (!validateEmail("customer-email")){
+            $("#customer-email-alert").removeClass("d-none");
+            $("#customer-email").val("");
+        }
+        else {
+            $("#customer-email-alert").addClass("d-none");
+        }
+    });
+
+    $("#customer-phone").on("change", function(){ // calls validatePhone and displays error alert if not valid
+        if (!validatePhone("customer-phone")){
+            $("#customer-phone-alert").removeClass("d-none");
+            $("#customer-phone").val("");
+        }
+        else {
+            $("#customer-phone-alert").addClass("d-none");
+        }
+    });
+
+    $("#customer-cc").on("change", function(){ // calls validateCreditCard and displays error alert if not valid
+        if (!validateCreditCard("customer-cc")){
+            $("#customer-cc-alert").removeClass("d-none");
+            $("#customer-cc").val("");
+        }
+        else {
+            $("#customer-cc-alert").addClass("d-none");
+        }
+    });
+
+    $("#customer-cvv").on("change", function(){ // calls validateCVV and displays error alert if not valid
+        if (!validateCVV("customer-cvv")){
+            $("#customer-cvv-alert").removeClass("d-none");
+            $("#customer-cvv").val("");
+        }
+        else {
+            $("#customer-cvv-alert").addClass("d-none");
+        }
+    });
+
+    $("#customer-cc-exp").on("change", function(){ // calls validateExpDate and displays error alert if not valid
+        if (!validateExpDate("customer-cc-exp")){
+            $("#customer-cc-exp-alert").removeClass("d-none");
+            $("#customer-cc-exp").val("");
+        }
+        else {
+            $("#customer-cc-exp-alert").addClass("d-none");
         }
     });
 
@@ -62,27 +167,4 @@ $(document).ready(function(){
             beforeShowDay: disableDates
         }   
     );
-
-
-    // Look at the different events on which an action can be performed
-    // https://www.w3schools.com/jquery/jquery_events.asp
-    // Here, we put 
-    $("#customer-cc").on("mouseenter", function(){
-        $("#customer-cc").addClass("showInput");
-    });
-
-    $("#customer-cc").on("mouseleave", function(){
-        $("#customer-cc").removeClass("showInput");
-    });
-  
-    // https://jqueryui.com/tooltip/ 
-    // The class "highlight" used here is predefined in JQuery UI
-    // the message of the tooltip is encoded in the input (in the HTML file)
-    $("#customer-cc").tooltip({
-        classes: {
-          "ui-tooltip": "highlight"
-        }
-      });
-
-
 });
